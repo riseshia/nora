79f33c17f5901e24996c206512bc519aaed34869
Shia <rise.shia@gmail.com>
2017-09-03 10:57:02 +0900

    Fix Omniauth load error

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,16 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: 'repo,write:repo_hook'
+require 'omniauth-github'
+
+Nora::Engine.config.middleware.use OmniAuth::Builder do
+  options = { scope: 'repo,write:repo_hook' }
+  host = ENV['GITHUB_HOST']
+  if host.present?
+    options.merge!(
+      client_options: {
+        site: "#{host}/api/v3",
+        authorize_url: "#{host}/login/oauth/authorize",
+        token_url: "#{host}/login/oauth/access_token",
+      }
+    )
+  end
+  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], options
 end
